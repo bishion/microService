1503f3302cf3adab51e96a1188538ce334e3f952
bishion <bishion@163.com>
2016-12-27 09:54:45 +0800

    configserver

---
+++ configclient/src/main/java/com/bizi/configclient/Application.java
@@ -0,0 +1,24 @@
+package com.bizi.configclient;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by guofangbi on 2016/12/26.
+ */
+@SpringBootApplication
+@RestController
+public class Application {
+    @Value("${server.name}") String luckyWord;
+    @RequestMapping("/printValue")
+    public String printValue(){
+        return luckyWord;
+    }
+    public static void main(String[] args) {
+//        System.setProperty("DEPLOY_ENV", "dev");
+        SpringApplication.run(Application.class,args);
+    }
+}
---
+++ configserver/src/main/java/com/bizi/configserver/Application.java
@@ -0,0 +1,16 @@
+package com.bizi.configserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ * Created by guofangbi on 2016/12/26.
+ */
+@SpringBootApplication
+@EnableConfigServer
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class,args);
+    }
+}
