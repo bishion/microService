bbe4a39aec31194cc9adf4a75527a278d3819c5b
bishion <bishion@163.com>
2017-01-12 19:53:56 +0800

    eureka

---
+++ metrics/src/main/java/com/bizi/metrics/Application.java
@@ -0,0 +1,36 @@
+package com.bizi.metrics;
+
+import com.codahale.metrics.MetricRegistry;
+import com.codahale.metrics.jvm.*;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.lang.management.ManagementFactory;
+
+/**
+ * Created by guofangbi on 2017/1/12.
+ */
+@SpringBootApplication
+@RestController
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class,args);
+    }
+    @RequestMapping("/test")
+    public String test(){
+        return "hello world";
+    }
+    @RequestMapping("/metrics")
+    public Object metrics(){
+        MetricRegistry metricRegistry = new MetricRegistry();
+
+        metricRegistry.register("jvm.buffers", new BufferPoolMetricSet(ManagementFactory.getPlatformMBeanServer()));
+        metricRegistry.register("jvm.cl", new ClassLoadingGaugeSet());
+        metricRegistry.register("jvm.gc", new GarbageCollectorMetricSet());
+        metricRegistry.register("jvm.memory", new MemoryUsageGaugeSet());
+        metricRegistry.register("jvm.threads", new ThreadStatesGaugeSet());
+        return metricRegistry.getMetrics();
+    }
+}
