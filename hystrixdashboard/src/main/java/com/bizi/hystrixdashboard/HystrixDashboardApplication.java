800357ba0da662e9d7c518cb7ef52e9e8604b6f3
bishion <bishion@163.com>
2017-06-02 20:50:20 +0800

    hystrix

--- hystrixdashboard/src/main/java/com/bizi/hystrixdashboard/HystrixDashboardApplication.java
+++ hystrixdashboard/src/main/java/com/bizi/hystrixdashboard/HystrixDashboardApplication.java
@@ -2,13 +2,16 @@ package com.bizi.hystrixdashboard;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+import org.springframework.cloud.netflix.turbine.EnableTurbine;
 
 /**
  * Created by guofangbi on 2017/6/1.
  */
 @SpringBootApplication
 @EnableHystrixDashboard
+@EnableTurbine
 public class HystrixDashboardApplication {
     public static void main(String[] args) {
         SpringApplication.run(HystrixDashboardApplication.class,args);
