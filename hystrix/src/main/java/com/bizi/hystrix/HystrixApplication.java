aa31c6952cab632c3ade1c31cbf0232b62e63556
bishion <bishion@163.com>
2017-08-23 20:32:18 +0800

    hystrix

--- hystrix/src/main/java/com/bizi/hystrix/HystrixApplication.java
+++ hystrix/src/main/java/com/bizi/hystrix/HystrixApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.turbine.EnableTurbine;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,6 +20,7 @@ import org.springframework.web.bind.annotation.RestController;
 @EnableHystrix
 @RestController
 @EnableEurekaClient
+@EnableTurbine
 public class HystrixApplication {
     public static void main(String[] args) {
         SpringApplication.run(HystrixApplication.class, args);
