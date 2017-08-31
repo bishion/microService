c607b9cd7dcd20a63767ff55605e5d00115aff27
bishion <bishion@163.com>
2017-08-31 15:03:04 +0800

    test

--- hystrixdashboard/src/main/java/com/bizi/hystrixdashboard/HystrixDashboardApplication.java
+++ hystrixdashboard/src/main/java/com/bizi/hystrixdashboard/HystrixDashboardApplication.java
@@ -2,14 +2,16 @@ package com.bizi.hystrixdashboard;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+import org.springframework.cloud.netflix.turbine.EnableTurbine;
 
 /**
  * Created by guofangbi on 2017/6/1.
  */
 @SpringBootApplication
 @EnableHystrixDashboard
-//@EnableTurbine
+@EnableTurbine
 public class HystrixDashboardApplication {
     public static void main(String[] args) {
         SpringApplication.run(HystrixDashboardApplication.class,args);
