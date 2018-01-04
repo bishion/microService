acdff812b9e219244b65cc413ba8d7ee8a338da7
guofangbi <guofangbi@zhongan.com>
2018-01-04 21:14:32 +0800

    zipkin

---
+++ sleuthzipkin/src/main/java/com/bizi/sleuth/zipkin/ZipkinApplication.java
@@ -0,0 +1,13 @@
+package com.bizi.sleuth.zipkin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import zipkin.server.EnableZipkinServer;
+
+@SpringBootApplication
+@EnableZipkinServer
+public class ZipkinApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ZipkinApplication.class,args);
+    }
+}
